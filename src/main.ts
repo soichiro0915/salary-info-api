f4483e69cd5bb714067238fb51eeee71208142dc
soichiro0915 <soichiro.ex.eru0915@gmail.com>
2022-12-28 17:48:12 +0900

    Auth service, controller実装

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // class-validator有効化かつ、whitelistを有効化（DTOに含まれていないものを省く）
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors({
     origin: 'http://localhost:3000',
